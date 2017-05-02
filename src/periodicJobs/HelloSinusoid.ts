65caf771c3c56bb2b73a785e367b44c6d0d6c2db
David Golverdingen <dgolverdingen@magion.nl>
2017-05-02 15:49:29 +0200

    Added class to job

--- src/periodicJobs/HelloSinusoid.ts
+++ src/periodicJobs/HelloSinusoid.ts
@@ -4,6 +4,8 @@ import "reflect-metadata";
 // Import interfaces for class implementation and dependency injection
 import { IPeriodicJob } from "../interfaces/IPeriodicJob";
 import { IPIWebAPIService } from "../interfaces/IPIWebAPIService";
+// Import classes from PiWebAPI for clearer Typescript Typings
+import { TimedValue } from "../apiclients/piwebapi";
 
 @injectable()
 export class HelloSinusoid implements IPeriodicJob {
@@ -22,7 +24,7 @@ export class HelloSinusoid implements IPeriodicJob {
     public async run(job: any, done: any) {
         try {
             // Await actual Sinusoid value from PIWebAPI service with basic auth defined in configs/piwebapi_config.ts
-            const result = await this.service.getPIPointDataByPath("\\\\PI2016\\SINUSOID");
+            const result: TimedValue = await this.service.getPIPointDataByPath("\\\\PI2016\\SINUSOID");
             // Log results to console
             console.log(`Hello Periodic Sinusoid with value ${result.Value} at timestamp ${result.Timestamp}`);
             // Call done function when job is finished
